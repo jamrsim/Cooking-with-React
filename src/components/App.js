fcbe0f250e86ac522d7f434f69a906f7c5b538c7
Jim Simpson <jsimpson@wwwilliams.com>
2020-10-30 16:55:10 -0400

    Video 15 Complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import RecipeList from './RecipeList'
 import '../css/app.css'
 import { v4 as uuidv4 } from 'uuid'
 
+export const RecipeContext = React.createContext()
+
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
-  
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
+
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
@@ -14,23 +21,22 @@ function App() {
       cookTime: '1:00',
       instructions: 'Instr.',
       ingredients: [
-        { id: uuidv4(), name: 'Name', amount: '1 Tbs'}
+        { id: uuidv4(), name: 'Name', amount: '1 Tbs' }
       ]
     }
-  
+
     setRecipes([...recipes, newRecipe])
   }
-  
+
   function handleRecipeDelete(id) {
     setRecipes(recipes.filter(recipe => recipe.id !== id))
   }
   return (
-    <RecipeList recipes={recipes}
-      handleRecipeAdd={handleRecipeAdd}
-      handleRecipeDelete={handleRecipeDelete}
-    />
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
   )
-  
+
 }
 
 const sampleRecipes = [
